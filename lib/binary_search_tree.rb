ac270bf17994088de750023c48b773196a7a3aa8
Jason Park <jjpark987@gmail.com>
2023-08-01 13:07:44 -0700

    commit tree cfb1abfe243966452fd67301f38d5fe70a739f62

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class BinarySearchTree
   attr_accessor :root
@@ -8,11 +9,53 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    return nil if @root.nil?
+
+    current_node = @root
+
+    while true
+      return nil if current_node.nil?
+
+      if current_node.value < value
+        current_node = current_node.right
+      elsif current_node.value > value
+        current_node = current_node.left
+      else
+        return current_node
+      end
+    end
   end
 
   def insert(value)
-    # your code here
+    return nil unless self.search(value).nil?
+
+    new_node = Node.new(value)
+
+    if @root.nil?
+      @root = new_node
+    else
+      current_node = @root
+
+      while true
+        if current_node.value < value
+          if current_node.right.nil?
+            current_node.right = new_node
+            break
+          else
+            current_node = current_node.right
+          end
+        else
+          if current_node.left.nil?
+            current_node.left = new_node
+            break
+          else
+            current_node = current_node.left
+          end
+        end
+      end
+    end
+
+    new_node
   end
 
 end 
